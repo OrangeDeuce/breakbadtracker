67cb378e27c58985cb132dfb3968c9b7294da558
Mike R <or4ngedeuce@gmail.com>
2022-08-03 12:43:17 -0500

    Import AuthForm into SignupScreen.js

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -3,6 +3,7 @@ import { View, StyleSheet, TouchableOpacity } from "react-native";
 import { Button, Text, Input } from "react-native-elements";
 import Spacer from "../components/Spacer";
 import { Context as AuthContext } from "../context/AuthContext";
+import AuthForm from "../components/authForm";
 
 const SignupScreen = ({ navigation }) => {
   const { state, signup } = useContext(AuthContext);
@@ -10,6 +11,12 @@ const SignupScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Spacer>
+        <AuthForm
+          headerText="Sign Up for Tracker"
+          errorMessage={state.errorMessage}
+          onSubmit={({ email, password }) => signup({ email, password })}
+          submitButtonText="Sign Up"
+        />
         <TouchableOpacity onPress={() => navigation.navigate("Signin")}>
           <Text style={styles.link}>
             Already have an account? Sign in instead.
